ada021e5a340898bc14589f9b5cafd6593c3b77e
Philip Davis <philipcdavis11@gmail.com>
2022-06-02 16:59:57 -0700

    Initial Commit

---
+++ Boomerang/BoomerangApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoomerangApp.swift
+//  Boomerang
+//
+//  Created by Philip Davis on 6/2/22.
+//
+
+import SwiftUI
+
+@main
+struct BoomerangApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
